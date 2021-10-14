48d14ea87e723f70742e2191f1e733cf1ca93cd7
Konrad Sikorski <konrad.sikorski@windowslive.com>
2021-10-15 00:42:12 +0200

    add MongoDb

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
 using Catalog.Repositories;
+using Catalog.Settings;
 using Microsoft.OpenApi.Models;
+using MongoDB.Bson.Serialization;
+using MongoDB.Bson.Serialization.Serializers;
+using MongoDB.Driver;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String)); // treat Guid type in entities as string
+BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
 
-builder.Services.AddSingleton<IItemsRepository, InMemItemsRepository>();
+// Add services to the container.
+builder.Services.AddSingleton<IMongoClient>(serviceProvider => {
+    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+    return new MongoClient(settings.ConnectionString);
+});
+
+builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen(c =>
 {
