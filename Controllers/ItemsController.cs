ab434032d969fcb9635f6e190463bf971618f36e
Konrad Sikorski <konrad.sikorski@windowslive.com>
2021-10-14 22:39:49 +0200

    first catalog

---
+++ Controllers/ItemsController.cs
@@ -0,0 +1,35 @@
+using Catalog.Entities;
+using Catalog.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly InMemItemsRepository repository;
+
+        public ItemsController()
+        {
+            repository = new InMemItemsRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Item> GetItems()
+        {
+            var items = repository.GetItems();
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Item> GetItems(Guid id)
+        {
+            var item = repository.GetItem(id);
+
+            if(item == null) return NotFound();
+
+            return item;
+        }
+    }
+}
