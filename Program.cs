9386178de617727236f8bb4f0b7179264a79c727
Konrad Sikorski <konrad.sikorski@windowslive.com>
2021-10-15 19:13:05 +0200

    add health check endpoint

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
+using System.Net.Mime;
+using System.Text.Json;
 using Catalog.Repositories;
 using Catalog.Settings;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.OpenApi.Models;
 using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Serializers;
@@ -10,10 +13,11 @@ var builder = WebApplication.CreateBuilder(args);
 BsonSerializer.RegisterSerializer(new GuidSerializer(MongoDB.Bson.BsonType.String)); // treat Guid type in entities as string
 BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(MongoDB.Bson.BsonType.String));
 
+var mongoDbSettings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
+
 // Add services to the container.
 builder.Services.AddSingleton<IMongoClient>(serviceProvider => {
-    var settings = builder.Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-    return new MongoClient(settings.ConnectionString);
+    return new MongoClient(mongoDbSettings.ConnectionString);
 });
 
 builder.Services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
@@ -24,6 +28,14 @@ builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "Catalog", Version = "v1" });
 });
+
+builder.Services.AddHealthChecks()
+    .AddMongoDb(
+        mongoDbSettings.ConnectionString, 
+        name: "mongodb", 
+        timeout: TimeSpan.FromSeconds(3),
+        tags: new[] {"ready"}
+        );
 
 var app = builder.Build();
 
@@ -38,6 +50,32 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseRouting();
+app.UseEndpoints( endpoint => {
+    endpoint.MapControllers();
+    endpoint.MapHealthChecks("/health/ready", new HealthCheckOptions{
+        Predicate = (check) => check.Tags.Contains("ready"),
+        ResponseWriter = async(context, report) =>
+        {
+            var result = JsonSerializer.Serialize(
+                new{
+                    status = report.Status.ToString(),
+                    checks = report.Entries.Select( entry => new {
+                        name = entry.Key,
+                        status = entry.Value.Status.ToString(),
+                        exception = entry.Value.Exception?.Message,
+                        duration = entry.Value.Duration.ToString()
+                    })
+                }
+            );
+
+            context.Response.ContentType = MediaTypeNames.Application.Json;
+            await context.Response.WriteAsync(result);
+        }
+    });
+    endpoint.MapHealthChecks("/health/live", new HealthCheckOptions{
+        Predicate = (_) => false
+    });
+});
 
 app.Run();
